e0275f4e09b544c61e6af5df4f50f19b3a6cbf0e
Bijon90 <bkbose@iastate.edu>
2016-11-02 13:15:19 -0500

    UI Screens
    Added Register, User Profile, Alert List

---
+++ MyApplication2/app/src/main/java/com/example/preeti/myapplication/AlertListActivity.java
@@ -0,0 +1,13 @@
+package com.example.preeti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlertListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_list);
+    }
+}
