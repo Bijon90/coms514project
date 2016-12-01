5af3d2ce3cac5bb94bd808a7011fbcdd1cc92460
Vijayadeepak <gbvijayadeepak@gmail.com>
2016-11-30 20:32:17 -0600

    Integrating chart

--- MyApplication2/app/src/main/java/com/example/preeti/myapplication/DisplayScreen.java
+++ MyApplication2/app/src/main/java/com/example/preeti/myapplication/DisplayScreen.java
@@ -1,21 +1,49 @@
 package com.example.preeti.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+/*import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.jjoe64.graphview.series.DataPoint;
+*/
+import java.util.Random;
+
+import com.github.mikephil.charting.charts.LineChart;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.Random;
-
 public class DisplayScreen extends AppCompatActivity {
+
+    //private RelativeLayout mainLayout;
+    //private LineChart mChart;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    //private GoogleApiClient client;
+
 
     private LineGraphSeries<DataPoint> series;
     private static final Random RANDOM=new Random();
@@ -23,19 +51,65 @@ public class DisplayScreen extends AppCompatActivity {
     private RelativeLayout mainLayout;
     private Button submit;
     private String url="http://www.fitbit.com"; //specify the URL here
-    //private LineChart mChart;
+    private LineChart mChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_screen);
+        /*
+        mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        mChart = new LineChart(this);
+        //mainLayout.addView(mChart);
+        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        mainLayout.addView(mChart, new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT));﻿
+        //mainLayout.addView(mChart,);﻿
+
+        //mChart.setDescription(" ");
+        mChart.setNoDataText("No data");
+
+        mChart.setHighlightPerTapEnabled(true);
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        mChart.setPinchZoom(true);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        mChart.setData(data);
+
+        Legend l = mChart.getLegend();
+
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+
+        YAxis yl = mChart.getAxisLeft();
+        yl.setTextColor(Color.WHITE);
+        yl.setAxisMaxValue(120f);
+        yl.setDrawGridLines(true);
+
+        YAxis yl2 = mChart.getAxisRight();
+        yl2.setEnabled(false);
+
+        */
+
+
         GraphView graph=(GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
         graph.addSeries(series);
         Viewport viewport = graph.getViewport();
         viewport.setYAxisBoundsManual(true);
-        viewport.setMinY(40);
+        viewport.setMinY(70);
         viewport.setMaxY(100);
-        //viewport.setScrollable(true);
+        viewport.scrollToEnd();
+        viewport.setScrollable(true);
         submit = (Button) findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -49,14 +123,34 @@ public class DisplayScreen extends AppCompatActivity {
             }
         });
 
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     protected void onResume()
     {
         super.onResume();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i=40;i<100;i++)
+                for(int i=0;i<100;i++)
                 {
                     runOnUiThread(new Runnable() {
                         @Override
@@ -65,7 +159,7 @@ public class DisplayScreen extends AppCompatActivity {
                         }
                     });
                     try {
-                        Thread.sleep(600);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -73,10 +167,74 @@ public class DisplayScreen extends AppCompatActivity {
             }
         }).start();
     }
-    private void addEntry()
-    {
-        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 100 ),true,10);
+
+    private void addEntry() {
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 100d ),true,40);
+        /* LineData data = mChart.getData();
+        if (data != null) {
+            LineDataSet set = (LineDataSet) data.getDataSetByIndex(0);
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+            //data.addXValue("");
+            data.addEntry(new Entry((float) (Math.random() * 75)+75f,set.getEntryCount()),0);
+            mChart.notifyDataSetChanged();
+            mChart.setVisibleXRange(0,10);
+            //mChart.moveViewTo(data.-7);
+        }*/
+
+    }
+    /*
+    private LineDataSet createSet() {
+        LineDataSet set=new LineDataSet(null, "Spl");
+        //set.setDrawCubic(true);
+        set.setCubicIntensity(0.2f);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(2f);
+        set.setFillAlpha(65);
+
+
+        return set;
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    /*
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("DisplayScreen Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+    */
 }
